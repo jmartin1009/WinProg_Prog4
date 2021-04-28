2c9798b557038a11d60713b3ecbdc94a8bcc72c6
Jaelen <Martinjae@uwplatt.edu>
2021-04-28 18:50:49 -0500

    Added DLL, finished program

--- Prog4/Prog4/Form1.vb
+++ Prog4/Prog4/Form1.vb
@@ -1,12 +1,22 @@
 ﻿Public Class Form1
-    Private db As New DB_Methods
     'Handles user clicking the show button
     Private Sub btnShow_Click(sender As Object, e As EventArgs) Handles btnShow.Click
-        db.fnShow(Me)
+        If (txtDBFilePath.Text = "" Or txtDBFilePath.Text = "Enter full file path for database file") Then
+            MessageBox.Show("Please enter a file path for the database you wish to access!!", "Windows Programming - Program 4",
+                            MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        Else
+            Dim dt = Prog4_DLL.fnShow(txtDBFilePath.Text)
+            Me.dvgEmployee.DataSource = dt
+        End If
     End Sub
     'Handles user clicking the delete button
     Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
-        db.fnDelete(txtDeleteID.Text, Me)
+        If (txtDBFilePath.Text = "" Or txtDBFilePath.Text = "Enter full file path for database file") Then
+            MessageBox.Show("Please enter a file path for the database you wish to access!!", "Windows Programming - Program 4",
+                            MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        Else
+            Prog4_DLL.fnDelete(txtDeleteID.Text, txtDBFilePath.Text)
+        End If
     End Sub
     'Handles user clicking the insert button
     Private Sub btnInsert_Click(sender As Object, e As EventArgs) Handles btnInsert.Click
@@ -14,7 +24,7 @@
             MessageBox.Show("Please enter a file path for the database you wish to access!!", "Windows Programming - Program 4",
                             MessageBoxButtons.OK, MessageBoxIcon.Warning)
         Else
-            db.fnInsert(txtID.Text, txtName.Text, Convert.ToInt32(txtSalary.Text), txtAddress.Text, Me)
+            Prog4_DLL.fnInsert(txtID.Text, txtName.Text, Convert.ToInt32(txtSalary.Text), txtAddress.Text, txtDBFilePath.Text)
         End If
     End Sub
 End Class
--- Prog4_DLL/Prog4_DLL/Class1.vb
+++ Prog4_DLL/Prog4_DLL/Class1.vb
@@ -1,13 +1,12 @@
-﻿Imports System
-Imports System.Data
-Imports System.Data.OleDb
-Public Class DB_Methods
+﻿Imports System.Data.OleDb
+
+Public Module Class1
     Private conFlag As Boolean = False
-    Private con As OleDbConnection = New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\employeeDB.mdb")
+    Private con As OleDbConnection = New OleDbConnection
     'Handles showing the database table contents
     'Contains code from Argo's example
-    Public Sub fnShow(sender As Form1)
-        connectionCheck()
+    Public Function fnShow(conString As String)
+        connectionCheck(conString)
         Dim cmd = doCommand("Select * from tblEmployee")
 
         'Fire the command using data adapter
@@ -16,15 +15,19 @@ Public Class DB_Methods
 
         'Fill it in a temporary table
         Dim dt As New DataTable
-        da.Fill(dt)
+        Try
+            da.Fill(dt)
+        Catch ex As Exception
+        End Try
 
-        'Display in the front end
-        sender.dvgEmployee.DataSource = dt
 
-    End Sub
+        'Return the datatable the form will show
+        Return dt
+
+    End Function
     'Handles deleting a row from the table based on ID entered
-    Public Sub fnDelete(userID As String, sender As Form1)
-        connectionCheck()
+    Public Sub fnDelete(userID As String, conString As String)
+        connectionCheck(conString)
         Dim cmd = doCommand("DELETE FROM tblEmployee WHERE empID='" & userID & "'")
         Try
             Dim check = cmd.ExecuteNonQuery
@@ -34,11 +37,10 @@ Public Class DB_Methods
         Catch ex As Exception
             MsgBox(ex.Message)
         End Try
-        fnShow(sender)
     End Sub
     'Handles adding a row to the table given the inputs from the user
-    Public Sub fnInsert(userID As String, userName As String, userSalary As Integer, userAddress As String, sender As Form1)
-        connectionCheck()
+    Public Sub fnInsert(userID As String, userName As String, userSalary As Integer, userAddress As String, conString As String)
+        connectionCheck(conString)
         Dim cmd = doCommand("INSERT INTO tblEmployee (empID, empName, empSalary, empAddress) VALUES ('" & userID & "', '" & userName & "', '" & userSalary & "', '" & userAddress & "')")
         Try
             Dim check = cmd.ExecuteNonQuery
@@ -48,7 +50,6 @@ Public Class DB_Methods
         Catch ex As Exception
             MsgBox(ex.Message)
         End Try
-        fnShow(sender)
     End Sub
     'Creates and returns a command given the query string
     Private Function doCommand(query As String)
@@ -58,12 +59,21 @@ Public Class DB_Methods
         Return cmd
     End Function
     'Checks connection to database, only opens connection if connection flag indicates a connection has not been made
-    Public Sub connectionCheck()
-        If (conFlag = False) Then
-            con.Open()
-            conFlag = True
-        End If
+    Public Sub connectionCheck(filePath As String)
         Try
+            Dim conString As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=#"
+            conString = conString.Replace("#", filePath)
+            If con.State = ConnectionState.Open Then
+                If con.ConnectionString <> conString Then
+                    conFlag = False
+                    con.Close()
+                End If
+            End If
+            If (conFlag = False) Then
+                con = New OleDbConnection(conString)
+                con.Open()
+                conFlag = True
+            End If
             If Not (con.State = ConnectionState.Open) Then
                 Throw New Exception("Database Connection failed")
             End If
@@ -71,4 +81,4 @@ Public Class DB_Methods
             MsgBox(ex.Message)
         End Try
     End Sub
-End Class
+End Module
